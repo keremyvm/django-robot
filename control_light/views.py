599ed8bce67ffb6c7ca6b22bc4d3c70d45f7cc90
kvergaray <kvergaray@xplora-sap.com>
2024-06-30 23:40:30 -0500

    sistema contra incendios ultimo trabajo

--- control_light/views.py
+++ control_light/views.py
@@ -3,45 +3,55 @@ import serial
 import json
 import time
 from django.http import JsonResponse
+import sys
 
 arduino = serial.Serial("COM5", 9600)
 
 def detectarConfiguracionArduino():
     return {
-        "pinAzul": 12,
-        "pinRojo": 11,
-        "servoMotPin": 3
+        "pinAzul": "12",
+        "pinRojo": "13",
+        "servoMotPin": "03"
     }
 
 def index(request) :
     return render(request, "control_light/index.html")
 
-def readGraph(request):
-    datos = arduino.readline().decode().strip()
-    objjson = json.loads(datos)
-    return JsonResponse(objjson)
+def leds(request, command) :
+    arduino.write((str(command) + ";" + str(request.POST.get("btn", ''))).encode())
+    time.sleep(1)
+    return render(request, "control_light/leds.html")
 
 def servo(request): 
     return render(request, "control_light/servo.html")
 
 
+
+def scaling(x, in_min, in_max, out_min, out_max) :
+    denominador = (float(in_max) - float(in_min)) + float(out_min)
+    numerador = (float(x) - float(in_min)) * (float(out_max) - float(out_min))
+    if (denominador <= 0.0):
+        return numerador
+    return float((numerador / denominador))
+
+
+def readGraph(request):
+    datos = (arduino.readline().decode().strip()).split(":")
+    volt = datos[0]
+    temp = datos[1]
+    # parsedTemp = scaling(float(temp), 0, 1023, 0, 50)
+    return JsonResponse({"temp": temp, "volt": volt})
+
 def enviarServo(request, command):
     configPinServo = detectarConfiguracionArduino()
-    jsonInfo = {
+    # jsonObj = json.dumps(jsonInfo,separators=(',', ':')).encode()
+    arduino.write(str(configPinServo["servoMotPin"] + ":" + command).encode())
+    
+    time.sleep(1)
+    return JsonResponse({
         "pin": configPinServo["servoMotPin"],
         "valor": command
-    }
-    
-    jsonObj = json.dumps(jsonInfo,separators=(',', ':')).encode()
-    arduino.write(jsonObj)
-    print(jsonObj)
-    time.sleep(1)
-    return JsonResponse(jsonInfo)
+    })
 
-def leds(request, command) :
-    print(command.encode())
-    arduino.write(str(command).encode())
-    time.sleep(1)
-        # arduino.close()
-    return render(request, "control_light/leds.html")
+
 
